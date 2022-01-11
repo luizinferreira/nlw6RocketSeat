bbbae4bb2be67202a72438de81cc651464dbbae6
Luiz <luizeduardo_2904@live.com>
2022-01-11 16:42:29 -0400

    Create pages Home and NewRooms

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,23 +1,11 @@
-import { useHistory } from 'react-router-dom';
-import { auth, firebase } from '../services/firebase'
+import { Link } from 'react-router-dom';
 
 import illustrationImg from '..//assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 import '../styles/auth.scss';
 
-export function Home() {
-    const history = useHistory();
-    function handleCreateRoom(){
-        const provider = new firebase.auth.GoogleAuthProvider();
-
-        auth.signInWithPopup(provider).then(result => {
-            console.log(result);
-            history.push('/rooms/new');
-        })
-    }
-
+export function NewRoom() {
     return(
         <div id="page-auth">
             <aside>
@@ -28,20 +16,19 @@ export function Home() {
             <main>
                 <div className="main-content">
                         <img src={logoImg} alt="Letmeask" />                    
-                    <button onClick={handleCreateRoom} className="create-room">
-                        <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua sala com o Google
-                    </button>
-                    <div className="separator">Ou entre em uma sala</div>
+                    <h2>Criar uma nova sala</h2>
                     <form>
                         <input
                             type="text"
-                            placeholder="Digite o código da sala"
+                            placeholder="Nome da Sala"
                         />
                         <Button type="submit">
-                            Entrar na Sala
+                            Criar Sala
                         </Button>
                     </form>
+                    <p>
+                        Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+                    </p>
                 </div>
             </main>
         </div>
